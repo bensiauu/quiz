87b3cbfc01121bf3ec181b783871fb436b0b187d
Benjamin <benjaminsiau@gmail.com>
2021-06-07 16:06:24 +0800

    part 1 complete

--- main.go
+++ main.go
@@ -5,21 +5,58 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
+type problem struct {
+	q string
+	a string
+}
+
 func main() {
-	csvFilename := flag.String("csv", "problems.csv", "csv file in format of questions.answer")
+	//help flag to let user know how to use
+	csvFileName := flag.String("csv", "problems.csv", "csv file in the format of question, answer")
 	flag.Parse()
-	file, err := os.Open(*csvFilename)
+
+	//open the specified csv
+	file, err := os.Open(*csvFileName)
 	if err != nil {
-		exit(fmt.Sprintf("Failed to open the CSV file: %s", *csvFilename))
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFileName))
+
 	}
+
+	//read the csv
 	r := csv.NewReader(file)
 	lines, err := r.ReadAll()
 	if err != nil {
 		exit("Failed to parse the provided CSV file")
 	}
-	fmt.Println(lines)
+	problems := parseLines(lines)
+
+	//number of correct answers
+	correct := 0
+	//
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.a {
+			correct++
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d\n", correct, len(problems))
+
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0], a: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
 }
 
 func exit(msg string) {
